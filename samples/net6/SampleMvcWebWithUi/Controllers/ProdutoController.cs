a451652a7074e3b770b11f4f460c6a8a071bb7dd
magsonleone2 <magsonleone@hotmail.com>
2023-04-30 09:52:55 -0400

    commit

---
+++ samples/net6/SampleMvcWebWithUi/Controllers/ProdutoController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SampleMvcWebWithUi.Models;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace SampleMvcWebWithUi.Controllers
+{
+    [Authorize]
+    public class ProdutoController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+
+        public ProdutoController(ILogger<HomeController> logger, RoleManager<ApplicationRole> roleManager)
+        {
+            _logger = logger;
+            _roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            //var role = new ApplicationRole { Name = "Test1" };
+            //var result = await _roleManager.CreateAsync(role);
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        public async Task<IActionResult> Batata()
+        {
+            return Ok("batata");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
